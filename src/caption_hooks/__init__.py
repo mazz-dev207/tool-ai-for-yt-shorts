95a27b445c841c10d0b6c5447a30cd5e92eb84a3
mazz-dev207 <mazzcomputers@gmail.com>
2026-08-13 03:17:56 +0300

    Add Caption Hook package

---
+++ src/caption_hooks/__init__.py
@@ -0,0 +1,4 @@
+from src.caption_hooks.generator import generate_caption_hook
+from src.caption_hooks.models import CaptionHookCandidate, CaptionHookResult
+
+__all__ = ["CaptionHookCandidate", "CaptionHookResult", "generate_caption_hook"]
